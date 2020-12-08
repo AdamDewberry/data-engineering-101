93c89e22ad5c332ecf481742e7f68e1a27a8f257
Adam Dewberry <38838996+AdamDewberry@users.noreply.github.com>
2020-12-08 19:11:06 +0000

    Remove demo code from python tests

--- code/python/test_file.py
+++ code/python/test_file.py
@@ -3,16 +3,3 @@ from file import double
 
 def test_double():
     assert double(2) == 4
-
-
-while True:
-    try:
-        x = int(input("Please enter a number: "))
-        break
-    except ValueError:
-        print(
-            "Oops!  That was no valid number.  That was no valid number.  That was no valid number.  That was no valid number.  Try again..."
-        )
-
-    the_next_funct()
-    print("a thing")
